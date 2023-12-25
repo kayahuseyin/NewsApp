ad8642ecf52ba7b17c13bfb4d1a027d6c79499d8
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2023-12-25 23:58:10 +0300

    initial commit

---
+++ NewsApp/View/NewsCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewsCell.swift
+//  NewsApp
+//
+//  Created by Hüseyin Kaya on 25.12.2023.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    @IBOutlet var newsImageView: UIImageView!
+    @IBOutlet var newsDescription: UILabel!
+    @IBOutlet var newsTitle: UILabel!
+    
+}
