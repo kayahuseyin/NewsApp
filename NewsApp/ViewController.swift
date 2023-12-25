ed701bad4f691d9d25ad926294cfed11709b6d79
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2023-12-25 22:34:20 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Hüseyin Kaya on 25.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
